9d828343979b752eab8d0c034e3e0eaa4ceab77f
Thomas Halvick <thomas.halvick@gmail.com>
2021-11-25 12:03:59 +0100

    route accueil

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,12 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="pro_stage")
      */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
+            'controller_name' => 'Conrôlleur prostage',
         ]);
     }
 }
